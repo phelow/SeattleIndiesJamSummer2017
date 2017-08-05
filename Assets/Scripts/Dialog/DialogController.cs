54228df418f447135bf69c69a064fb63d5660162
LAPTOP\Bamboy <bamboy360@gmail.com>
2017-08-05 16:49:32 -0700

    Added speech bubble UI

--- Assets/Scripts/Dialog/DialogController.cs
+++ Assets/Scripts/Dialog/DialogController.cs
@@ -38,11 +38,33 @@ public class DialogController : MonoBehaviour
 	{
 		display = GetComponent<DisplayString>();
 		followTarget = GetComponent<ObjectUI>();
+		followTarget.owner = this;
 	}
 
 	public int count{ get{ return participants.Count; } }
 
+	public Vector3 avgPosition
+	{
+		get{
+			//List<Vector3> positions = new List<Vector3>();
+			Vector3 sum = Vector3.zero;
+			for (int i = 0; i < participants.Count; i++) 
+			{
+				sum += participants[i].transform.position;
+			}
+			return sum / participants.Count;
+		}
+	}
+
 	public List<DialogParticipant> participants;
+	private DialogParticipant _talking;
+	public DialogParticipant talking
+	{
+		get{ return _talking; }
+		set{
+			_talking = value;
+		}
+	}
 
 	public void Add( DialogParticipant newParticipant )
 	{
@@ -61,6 +83,7 @@ public class DialogController : MonoBehaviour
 
 				if( this.table < DialogDefs.singleton.tables.Count )
 				{
+					this.talking = participants[ UnityEngine.Random.Range(0, participants.Count) ];
 					this.display.StartDisplay( DialogDefs.singleton.SelectRandom(table).text );
 				}
 			};
@@ -69,12 +92,28 @@ public class DialogController : MonoBehaviour
 
 	public void StartConversation()
 	{
+		if( DialogDefs.singleton == null )
+			Debug.Log("u dun goof");
+
 		table = 0;
 
-		if( DialogDefs.singleton == null )
-			Debug.Log("aaa");
 		display.onComplete = recursive;
+		this.talking = participants[ UnityEngine.Random.Range(0, participants.Count) ];
 		display.StartDisplay( DialogDefs.singleton.SelectRandom(table).text );
 	}
 
+	public bool IsParticipating( DialogParticipant person )
+	{
+		for (int i = 0; i < participants.Count; i++) 
+		{
+			if( participants[i] == person )
+				return true;
+		}
+		return false;
+	}
+
+	void Update()
+	{
+		followTarget.targetPos = avgPosition;
+	}
 }
--- Assets/Scripts/Dialog/ObjectUI.cs
+++ Assets/Scripts/Dialog/ObjectUI.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Speech UI
+/// </summary>
 public class ObjectUI : MonoBehaviour 
 {
-	public Transform followObj;
+	public DialogController owner;
+
+	public Vector3 targetPos;
 	public Vector3 offset;
+
+	public float padding = 30f;
+
+	private RectTransform speechWindow;
+	public RectTransform speechTriangle;
+
+	public float triangleHeightOffset = -55f;
+
+	void Awake()
+	{
+		speechWindow = GetComponent<RectTransform>();
+	}
 
 	void LateUpdate () 
 	{
-		if( followObj != null )
-			transform.position = Camera.main.WorldToScreenPoint( new Vector3(followObj.position.x, followObj.position.y, 0f) + offset  );
+
+		Vector3 screenPos = Camera.main.WorldToScreenPoint( new Vector3(targetPos.x, targetPos.y, 0f) );
+		transform.position = screenPos + offset;
+
+		Vector3[] bounds = new Vector3[4];
+		speechWindow.GetWorldCorners( bounds );
+
+		float min = bounds[0].x + padding;
+		float max = bounds[3].x - padding;
+
+
+		Debug.Log( min );
+		Debug.Log( max );
+
+		float x = Mathf.Clamp( Camera.main.WorldToScreenPoint( owner.talking.transform.position ).x, min, max );
+
+		Debug.Log( x );
+
+		speechTriangle.transform.position = new Vector3( x, bounds[0].y + triangleHeightOffset, 0f );
+
 	}
 }
