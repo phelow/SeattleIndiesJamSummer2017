059ef25f9cd6133d4fa8efc0e2d9949a3a1f47d7
LAPTOP\Bamboy <bamboy360@gmail.com>
2017-08-05 13:40:58 -0700

    Unfinished dialog system, Added text mesh pro

---
+++ Assets/Scripts/Dialog/DisplayString.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DisplayString : MonoBehaviour 
+{
+	private AudioSource dialogAudio;
+	private Text display;
+
+	void Awake () 
+	{
+		display = GetComponentInChildren<Text>();
+		dialogAudio = GetComponent<AudioSource>();
+
+		StartCoroutine(Display());
+	}
+
+	public float characterDelay = 0.1f;
+	private string _fullText = "Seattle Indies Jam August 2017 ";
+	public string text
+	{
+		get{ return _fullText; }
+		set{
+			charIndex = 0;
+			_fullText = value + " ";
+
+			StopCoroutine( routine );
+			routine = StartCoroutine( Display() );
+		}
+	}
+	private Coroutine routine;
+
+	public int charIndex = 0;
+	public Action onComplete = null;
+
+	public void StartDisplay( string text )
+	{
+		this.text = text;
+	}
+
+	IEnumerator Display()
+	{
+		yield return null; //Skip a frame so other scripts have time to initalize
+
+		for (int i = 0; i < _fullText.Length; i++) 
+		{
+			charIndex = i;
+
+
+			DisplayPartial( _fullText, charIndex );
+
+			yield return new WaitForSeconds( characterDelay );
+		}
+
+		yield return new WaitForSeconds( characterDelay );
+
+		if( onComplete != null )
+			onComplete();
+	}
+
+	void DisplayPartial( string full, int charCount )
+	{
+		display.text = full.Remove( charCount );
+	}
+}
