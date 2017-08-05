4d873b45aad86ee4a3b52110a7bbeb6366bef015
chaosed0 <chaosed0@gmail.com>
2017-08-04 22:04:52 -0700

    Commit things

---
+++ Assets/CityGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityGenerator : MonoBehaviour
+{
+    private CityPathing cityPathing;
+    public Transform transformPrefab;
+
+    void Awake()
+    {
+        cityPathing = GetComponent<CityPathing>();
+    }
+
+    void Start()
+    {
+        //DEBUG
+        for (int i = 0; i < 50; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(0, cityPathing.GetSize().x), 0, Random.Range(0, cityPathing.GetSize().y));
+            Transform person = Instantiate<Transform>(transformPrefab, this.transform, false);
+            person.localPosition = position;
+            person.GetComponent<Movement>().pathing = cityPathing;
+        }
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public CityPathing pathing;
+    public float moveSpeed;
+
+    private Vector3 targetPosition;
+    private Rigidbody rb;
+    private Vector2 direction;
+
+	void Start () {
+        if (pathing == null) {
+            pathing = Object.FindObjectOfType<CityPathing>();
+        }
+        
+        rb = GetComponent<Rigidbody>();
+
+        int random = (int)Random.Range(0, 2);
+        direction = new Vector2(random, random == 1 ? 0 : 1);
+        targetPosition = pathing.GetDestination(transform.position, direction);
+        Debug.Log(direction);
+    }
+
+    void Update () {
+        Vector3 rel = targetPosition - transform.position;
+        if (rel.magnitude < 0.1f) {
+            targetPosition = pathing.GetDestination(transform.position, direction);
+            rel = targetPosition - transform.position;
+            direction = new Vector2(rel.x, rel.y);
+            Debug.Log(direction);
+        }
+
+        rb.velocity = rel.normalized * moveSpeed;
+    }
+}
