675362e42d19b3cf902613aae86663552d447aaa
phelow <wpheloung@gmail.com>
2017-08-04 21:12:06 -0700

    Draw the rays

--- Assets/SpaceShipMovement.cs
+++ Assets/SpaceShipMovement.cs
@@ -12,6 +12,12 @@ public class SpaceShipMovement : MonoBehaviour
 
     [SerializeField]
     private float _movementForce = 100.0f;
+
+    [SerializeField]
+    private GameObject p_fervorGoo;
+
+    [SerializeField]
+    private GameObject p_harvestGoo;
     // Use this for initialization
     void Start()
     {
@@ -23,9 +29,10 @@ public class SpaceShipMovement : MonoBehaviour
     {
         bool leftPressed = Input.GetMouseButton(0);
         bool rightPressed = Input.GetMouseButton(1);
-                //if outside the circle move
+        //if outside the circle move
 
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float distance = Vector2.Distance(transform.position, worldPos);
 
         if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.01f || Mathf.Abs(Input.GetAxis("Horizontal")) > 0.01f)
         {
@@ -34,20 +41,38 @@ public class SpaceShipMovement : MonoBehaviour
 
         Vector2 movement = (new Vector2(worldPos.x, worldPos.y) - new Vector2(transform.position.x, transform.position.y));
 
-        if ((leftPressed || rightPressed) && _conversionRadius < Vector2.Distance(transform.position,worldPos))
+        if ((leftPressed || rightPressed) && _conversionRadius < distance)
         {
             _rigidbody.AddForce(movement.normalized * _movementForce * Time.deltaTime);
 
             return;
         }
 
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, movement, _conversionRadius);
+        Debug.DrawLine(transform.position, movement, Color.red);
+        if (hit == null || hit.transform == null || hit.transform.GetComponent<Consumable>() == null)
+        {
+            return;
+        }
+
+        Line2D.Line2DRenderer renderer = null;
+
         if (leftPressed)
         {
             //if inside the circle convert
+            renderer = GameObject.Instantiate(p_fervorGoo, hit.collider.transform.position, transform.rotation, null).GetComponent<Line2D.Line2DRenderer>();
+
+            renderer.points = new List<Line2D.Line2DPoint>();
+            renderer.points.Add(new Line2D.Line2DPoint(transform.position, .1f, Color.red));
+            renderer.points.Add(new Line2D.Line2DPoint(hit.collider.transform.position, .1f, Color.red));
         }
         else if (rightPressed)
         {
             //if inside the circle harvest
+            renderer = GameObject.Instantiate(p_fervorGoo, hit.collider.transform.position, transform.rotation, null).GetComponent<Line2D.Line2DRenderer>();
+            renderer.points = new List<Line2D.Line2DPoint>();
+            renderer.points.Add(new Line2D.Line2DPoint(transform.position, .1f, Color.blue));
+            renderer.points.Add(new Line2D.Line2DPoint(hit.collider.transform.position, .1f, Color.blue));
         }
     }
 }
