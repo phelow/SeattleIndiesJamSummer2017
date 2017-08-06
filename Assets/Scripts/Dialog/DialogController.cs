9b2e38975a4ef1936781ad2324c3f2c26be1055c
Luna Flesher <jamgrrl@gmail.com>
2017-08-06 15:06:44 -0700

    Revert "Another file to revert."
    This reverts commit 965953a9441083264bdf81682f6cd6550c548beb.

--- Assets/Scripts/Dialog/DialogController.cs
+++ Assets/Scripts/Dialog/DialogController.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(DisplayString), typeof(ObjectUI))]
 public class DialogController : MonoBehaviour 
 {
+    private const int maxDialogs = 5;
 	private const string prefabLocation = "Prefabs/UI/Conversation Window";
+    private static int dialogCount = 0;
 	public static GameObject prefab;
 	public static Transform canvas;
 
 	public static DialogController Create( params DialogParticipant[] participants )
 	{
+        if (dialogCount >= maxDialogs) {
+            return null;
+        }
+
 		if( prefab == null )
 			prefab = Resources.Load<GameObject>( prefabLocation );
 		if( canvas == null )
-			canvas = FindObjectOfType<Canvas>().transform;
+			canvas = GameObject.Find("MainCanvas").transform;
 
 		//TODO create script for obj positions
 
@@ -28,6 +35,8 @@ public class DialogController : MonoBehaviour
 		}
 
 		dc.StartConversation();
+
+        ++dialogCount;
 
 		return dc;
 	}
@@ -46,10 +55,17 @@ public class DialogController : MonoBehaviour
 	public Vector3 avgPosition
 	{
 		get{
-			//List<Vector3> positions = new List<Vector3>();
-			Vector3 sum = Vector3.zero;
+            participants = participants.Where(i => i != null).ToList();
+
+            //List<Vector3> positions = new List<Vector3>();
+            Vector3 sum = Vector3.zero;
 			for (int i = 0; i < participants.Count; i++) 
 			{
+                if(participants[i] == null)
+                {
+                    continue;
+                }
+
 				sum += participants[i].transform.position;
 			}
 			return sum / participants.Count;
@@ -60,7 +76,7 @@ public class DialogController : MonoBehaviour
 	private DialogParticipant _talking;
 	public DialogParticipant talking
 	{
-		get{ return _talking; }
+		get{return _talking; }
 		set{
 			_talking = value;
 		}
@@ -74,13 +90,30 @@ public class DialogController : MonoBehaviour
 
 	public int table = 0;
 
-	public Action recursive
+	public Func<bool> recursive
 	{
 		get{
 			return delegate() 
 			{
 				this.table++;
 
+<<<<<<< HEAD
+                if (participants.Count < 2)
+                {
+                    --dialogCount;
+                    return false;
+                }
+
+                if (this.table < DialogDefs.singleton.tables.Count)
+                {
+                    this.talking = (table % 2 == 0) ? participants[0] : participants[1]; //participants[ UnityEngine.Random.Range(0, participants.Count) ];
+                    this.display.StartDisplay(DialogDefs.singleton.SelectRandom(table).text);
+                    return true;
+                }
+
+                --dialogCount;
+                return false;
+=======
 				if( this.table < DialogDefs.singleton.tables.Count )
 				{
 					this.talking = (table % 2 == 0) ? participants[0] : participants[1]; //participants[ UnityEngine.Random.Range(0, participants.Count) ];
@@ -88,6 +121,7 @@ public class DialogController : MonoBehaviour
 				}
 				else
 					Debug.Log("end");
+>>>>>>> b24a571... Stuff
 			};
 		}
 	}
