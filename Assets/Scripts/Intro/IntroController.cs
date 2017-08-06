05c8231514ab8e13bb0459bab4ea7f3a4faa6add
chaosed0 <chaosed0@gmail.com>
2017-08-06 15:30:32 -0700

    Intro done

---
+++ Assets/Scripts/Intro/IntroController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class IntroController : MonoBehaviour {
+    [SerializeField]
+    UnityEngine.UI.Image _image;
+
+    [SerializeField]
+    GoToNext _goToNext;
+
+    void Start() {
+        _goToNext.enabled = false;
+    }
+
+    // Update is called once per frame
+	void Update () {
+	    if(Input.anyKeyDown || Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            _image.gameObject.SetActive(false);
+            _goToNext.enabled = true;
+        }
+	}
+}
