cfccca94501124a65c66bc34ec0c886d7014eaf1
phelow <wpheloung@gmail.com>
2017-08-06 10:08:15 -0700

    meemz

---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    [SerializeField]
+    private Text _text;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(CountUp());
+	}
+
+    private IEnumerator CountUp()
+    {
+        for(int i = 0; true; i++)
+        {
+            _text.text = i/60 + ":"+("" + i%60).PadLeft(2,'0');
+            PlayerPrefs.SetInt("CurrentTime", i);
+            PlayerPrefs.SetString("CurrentTimeString",_text.text);
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/WinTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTimer : MonoBehaviour {
+    [SerializeField]
+    private Text _text;
+	// Use this for initialization
+	void Start () {
+        
+        _text.text = "Your Time:" + PlayerPrefs.GetString("CurrentTimeString","1:10") + " Best Time:" + PlayerPrefs.GetString("LowTimeString","9:55");
+
+        if (PlayerPrefs.GetInt("CurrentTime") < PlayerPrefs.GetInt("LowTime",9999))
+        {
+            PlayerPrefs.SetString("LowTimeString", PlayerPrefs.GetString("CurrentTimeString"));
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
