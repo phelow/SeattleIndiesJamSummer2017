4f561788433337ee64aa01a3861f3af120b55b36
phelow <wpheloung@gmail.com>
2017-08-05 19:09:22 -0700

    LineRenderer Workaround

--- Assets/SpringyShackle.cs
+++ Assets/SpringyShackle.cs
@@ -7,9 +7,17 @@ public class SpringyShackle : MonoBehaviour {
     private Line2D.Line2DRenderer line;
 
 	// Use this for initialization
-	void Start () {
+
+    public void Update()
+    {
+        if(line.points == null || line.points.Count < 2)
+        {
+            line.points = new List<Line2D.Line2DPoint>();
+            line.points.Add(new Line2D.Line2DPoint(new Vector3(99999.0f, 99999.0f), .1f, Color.black));
+
+        }
     }
-	
+
     public void StartShackling(GameObject target)
     {
         StartCoroutine(LineRoutine(target));
@@ -28,7 +36,9 @@ public class SpringyShackle : MonoBehaviour {
         while (true)
         {
             line.points[0].pos = target.transform.position;
+            line.points[0].width = .1f;
             line.points[1].pos = transform.position;
+            line.points[1].width = .1f;
             yield return new WaitForFixedUpdate();
         }
     }
