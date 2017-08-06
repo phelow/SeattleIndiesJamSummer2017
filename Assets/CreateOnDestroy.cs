7fa8816fb44c02dde5e5d1f0d2f3f0930b47c6bd
chaosed0 <chaosed0@gmail.com>
2017-08-06 13:13:27 -0700

    Fix bug in create on destroy

--- Assets/CreateOnDestroy.cs
+++ Assets/CreateOnDestroy.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreateOnDestroy : MonoBehaviour {
     public GameObject[] go;
+    private bool isQuitting;
+
+    void OnApplicationQuit()
+    {
+        isQuitting = true;
+    }
 
     void OnDestroy()
     {
+        if (isQuitting)
+        {
+            return;
+        }
+
         for (int i = 0; i < go.Length; i++)
         {
             Instantiate(go[i], transform.position, transform.rotation, null);
